5ea9a01b7e3d7349fd8775142e169ebc082f6e0e
lucassherwin <lksherwin@gmail.com>
2022-11-20 13:49:10 -0500

    end section 2

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,47 @@
+const Fs = require('fs')
+const Path = require('path')
+const Sass = require('node-sass')
+
+const getComponents = () => {
+  let all_components = []
+
+  const types = ['atoms', 'molecules', 'organisms']
+
+  types.forEach(type => {
+    const all_files = Fs.readdirSync(`src/${type}`).map(file => ({
+      input: `src/${type}/${file}`,
+      output: `src/lib/${file.slice(0, -4) + 'css'}`
+    }))
+
+    all_components = [
+      ...all_components,
+      ...all_files
+    ]
+  })
+
+  return all_components
+}
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(
+      Path.resolve(path)
+    ).toString(),
+    outputStyle: 'expanded',
+    outFile: 'global.scss',
+    includePaths: [Path.resolve('src')]
+  })
+
+  Fs.writeFileSync(
+    Path.resolve(fileName),
+    result.css.toString()
+  )
+}
+
+compile('src/global.scss', 'src/lib/global.css')
+
+console.log(getComponents())
+
+getComponents().forEach(component => {
+  compile(component.input, component.output)
+})
